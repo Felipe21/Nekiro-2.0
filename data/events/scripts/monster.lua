9dbeefdf1128a7ad351d91c82c7cc29f0b2b4bcc
Felipe21 <felipeneug@gmail.com>
2023-02-28 03:51:18 -0300

    fix monster loot message

--- data/events/scripts/monster.lua
+++ data/events/scripts/monster.lua
@@ -1,9 +1,9 @@
 function Monster:onDropLoot(corpse)
 
 	local player = Player(corpse:getCorpseOwner())
-	if player then
-		player:updateKillTracker(self, corpse)
-	end
+	--if player then
+	--	player:updateKillTracker(self, corpse)
+--	end
 	if EventCallback.onDropLoot then
 		EventCallback.onDropLoot(self, corpse)
 	end
